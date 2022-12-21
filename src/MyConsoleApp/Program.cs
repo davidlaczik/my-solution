ac02730d002a730041a0c8d1a2a0f8c9f6ef1532
Dávid Laczik <david@Dvids-MacBook.local>
2022-12-21 02:55:26 +0100

    Initial commit

---
+++ src/MyConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.AccessControl;
+using System.Collections.Generic;
+using System.Linq;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("MyConsoleApp");
+
+
+var p = new Person()
+{
+    Id = new Guid()
+};
+
+var col = new List<int>();
+var col2 = new List<int>();
+
+var j = col.Join(col2, o => )
+
+class Person
+{
+    public Guid Id { get; set; }
+}
